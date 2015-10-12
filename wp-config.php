3f03a66ed14e8b472d58fdae1b2c142725d71f67
Michael Roberts Jr <michael.e.roberts.jr@gmail.com>
2015-10-12 00:31:10 -0700

    sets the basic multisite config

--- wp-config.php
+++ wp-config.php
@@ -15,6 +15,13 @@ define('FS_METHOD', 'direct');
 
 define('WP_ALLOW_MULTISITE', true);
 
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
+define('DOMAIN_CURRENT_SITE', 'zappi-dev.codemean.com');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
 $table_prefix  = 'wp_';
 
 define('WP_DEBUG', false);
